f8c4dad29e4499548fa9ae1593b09a93f68b8855
chriswang1990 <1990chriswang1990@gmail.com>
2016-07-07 13:13:22 -0700

    more gems and directives added

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 (function(){
 	var app = angular.module('store', []);
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
+	var gems = [
+	{
 		name: 'Dodecahedron',
 		price: 2.95,
-		description: 'Good Gem that you should always buy one!',
-		canPurchase: true
-	}
+		description: 'Good gem that you should buy one!',
+		canPurchase: true,
+		soldOut: true
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: 'Better gem that you should always buy one',
+		canPurchase: false
+	}]
 })();

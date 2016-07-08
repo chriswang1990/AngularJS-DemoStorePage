4212cbb7112ddeb138582c1db24fd330e4d7d795
chriswang1990 <1990chriswang1990@gmail.com>
2016-07-07 22:58:16 -0700

    Add the review content and submit form

--- app.js
+++ app.js
@@ -17,6 +17,13 @@
     };
   });
 
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
+  });
+
   var gems = [
     {
       name: 'Azurite',

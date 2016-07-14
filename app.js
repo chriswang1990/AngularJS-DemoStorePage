ef052d8a188b4662bdb5011e9845afa5b030a4ca
chriswang1990 <1990chriswang1990@gmail.com>
2016-07-13 23:14:12 -0700

    Seperate all the product directives to product.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
 
   app.controller('StoreController', function(){
     this.products = gems;
@@ -25,59 +25,6 @@
       this.review = {};
     };
   });
-
-  app.directive('productTabs', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-tabs.html',
-      controller: function(){
-        this.tab = 1;
-        this.isSet = function(checkTab) {
-          return this.tab === checkTab;
-        };
-        this.setTab = function(setTab) {
-          this.tab = setTab;
-        };
-      },
-      controllerAs: 'tab'
-    };
-  });
-
-  app.directive('productGallery', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-gallery.html',
-      controller: function(){
-        this.current = 0;
-        this.setCurrent = function(imageNumber){
-        this.current = imageNumber || 0;
-        };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.directive('productDescription', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-description.html'
-    };
-  });
-
-  app.directive('productReviews', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'product-reviews.html'
-    };
-  });
-
-  app.directive('productSpecs', function() {
-    return {
-      restrict:'A',
-      templateUrl: 'product-specs.html'
-    };
-  });
-
 
   var gems = [
     {

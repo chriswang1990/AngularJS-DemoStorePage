5da5debbb7165745dd853c1ee962ce178a8cdaa9
chriswang1990 <1990chriswang1990@gmail.com>
2016-07-13 20:10:47 -0700

    Add customized directive to make html explicit

--- app.js
+++ app.js
@@ -17,21 +17,67 @@
     };
   });
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
   app.controller('ReviewController', function() {
     this.review = {};
-    this.review.createdOn = date.now();
+    this.review.createdOn = Date.now();
     this.addReview = function(product) {
       product.reviews.push(this.review);
       this.review = {};
     };
   });
+
+  app.directive('productTabs', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function(){
+        this.tab = 1;
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  });
+
+  app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function(){
+        this.current = 0;
+        this.setCurrent = function(imageNumber){
+        this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
+  app.directive('productDescription', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
+
+  app.directive('productReviews', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-reviews.html'
+    };
+  });
+
+  app.directive('productSpecs', function() {
+    return {
+      restrict:'A',
+      templateUrl: 'product-specs.html'
+    };
+  });
+
 
   var gems = [
     {
